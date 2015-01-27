d9dc6958d49cde5c38147ca1ecc46442441e142a
eli_and_levi <eli_and_levi@appacademy.io>
2015-01-27 14:12:31 -0500

    find slidingPieces legal moves

--- pieces.rb
+++ pieces.rb
@@ -7,32 +7,49 @@ class Piece
     @moves = []
   end
 
-  def moves
-    # possible_moves is a method that returns array of possible moves
-      moves = possible_moves
-
-
-      moves = moves.select{}
-  end
+  # def legal_moves
+  #   # possible_moves is a method that returns array of possible moves
+  #     moves = pos_moves
+  #
+  #
+  #     moves = moves.select{}
+  # end
 end
 
 class SlidingPiece < Piece
   # Bishop, Queen, Rook
 
-  def initialize(*directions) # :rows, :cols, :diags
+  def initialize(*directions) # :rows, :cols, :diag
     super(board, color, pos)
     @directions = directions
   end
 
-  def pos_moves
-    get_direction
-    directions.each do |direction|
+  def legal_moves
+    legal_moves_arr = []
+    pos_moves.each do |direction_array|
+      direction_array.each do |one_direction_arr|
+        one_direction_arr.each do |tile|
+          if board[tile].nil?
+            legal_moves_arr << tile
+          elsif board[tile].color != color
+            legal_moves_arr << tile
+            break
+          else
+            break
+          end
+        end
+      end
     end
 
+    legal_moves_arr
   end
 
-  def get_direction
-    # @directions.each{|direction| direction_hash[direction] = []}
+  def pos_moves
+    possible_moves = []
+    directions.each do |direction|
+      possible_moves << @direction_hash[direction]
+    end
+    possible_moves
   end
 
   def create_deltas
@@ -40,31 +57,50 @@ class SlidingPiece < Piece
     @direction_hash[:rows] = get_row
     @direction_hash[:cols] = get_col
     @direction_hash[:diag1], @direction_hash[:diag2] = get_diags
+
   end
 
   def get_row
-    row = []
-    board.rows[pos[0]].each_index{|col| row << [pos[0], col]}
+    row = Array.new(2) {[]}
+    board.rows[pos[0]].each_index do |col|
+      col < pos[1] ? row[0] << [pos[0], col] : row[1] << [pos[0], col]
+    end
+
+    # puts row[0] in order of closness
+    row[0].reverse!
+
     row
   end
 
   def get_col
-    col = []
-    board.rows.each_index{|row| col << [row, pos[1]]}
+    row = Array.new(2) {[]}
+    board.rows.each_index do |row|
+      row < pos[0] ? col[0] << [row, pos[1]] : col[1] << [row, pos[1]]
+    end
+
+    # puts col[0] in order of closness
+    col[0].reverse!
+
     col
   end
 
-  def get_diags
-    diag1 = []
-    diag2 = []
-    8.times do |num|
-      diag1 << [num, num]
-      diag1 << [-num, -num]
-      diag2 << [-num, num]
-      diag2 << [num, -num]
-    end
-    [diag1.sort, diag2.sort]
+  def get_diags(pos)
+    diag = Array.new(4) {[]}
 
+    (1..7).each do |num|
+      diag[0] << [pos[0] + num, pos[1] + num]
+      diag[1] << [pos[0] - num, pos[1] - num]
+      diag[2] << [pos[0] - num, pos[1] + num]
+      diag[3] << [pos[0] + num, pos[1] - num]
+    end
+
+    # filter out of bounds
+    diag[0].select!{|el| el[0].between?(0,7) && el[1].between?(0,7)}
+    diag[1].select!{|el| el[0].between?(0,7) && el[1].between?(0,7)}
+    diag[2].select!{|el| el[0].between?(0,7) && el[1].between?(0,7)}
+    diag[3].select!{|el| el[0].between?(0,7) && el[1].between?(0,7)}
+
+    diag
   end
 
 end
