5496e96e70cece5911111d44f4d4c0c77b4d935d
eli_and_levi <eli_and_levi@appacademy.io>
2015-01-27 12:33:17 -0500

    Added deltas for rows and cols in delta hash

--- pieces.rb
+++ pieces.rb
@@ -18,14 +18,51 @@ end
 
 class SlidingPiece << Piece
   # Bishop, Queen, Rook
-  def initialize(*directions) # :rows, :columns, :diagonals
+
+  def initialize(*directions) # :rows, :cols, :diags
     super(board, color, pos)
     @directions = directions
   end
 
   def pos_moves
+    get_direction
     directions.each do |direction|
     if (:rows)
+  end
+
+  def get_direction
+    # @directions.each{|direction| direction_hash[direction] = []}
+  end
+
+  def create_deltas
+    @direction_hash = {} #Hash.new{|h,k| h[k] = []}
+    @direction_hash[:rows] = get_row
+    @direction_hash[:cols] = get_col
+    @direction_hash[:diags]
+  end
+
+  def get_row
+    row = []
+    board.rows[pos[0]].each_index{|col| row << [pos[0], col]}
+    row
+  end
+
+  def get_col
+    col = []
+    board.rows.each_index{|row| col << [row, pos[1]]}
+    col
+  end
+
+  def get_diags
+    diag1 = []
+    diag2 = []
+    8.times do |row|
+      8.times do |col|
+        # ??
+      end
+    end
+
+
   end
 
 end
